8cd3c0142f8c3521b2614422de12c91f108659ef
Jhon Marreros Guzman <j_marreros@hotmail.com>
2018-01-21 19:59:34 -0500

    Avance Cambio a Jquery

--- admin/js/video-list-for-courses-admin.js
+++ admin/js/video-list-for-courses-admin.js
@@ -1,40 +1,144 @@
 
 // template
-const template = `
-<li class="item">
-	<div class="links my-handle menu-item-handle">
-		<a href="#" class="item-link"></a>
-		<a href="#" class="item-arrow down"></a>
-	</div>
-	<div class="controls hide">
-		<p class="control-name">
-			<label>Item name</label><input type="text" value=""/>
-		</p>
-		<p class="control-check-header">
-			<input type="checkbox"><label>Is header?</label>
-		</p>
-		<p class="control-code">
-			<label>Video Embebed code</label><textarea cols="30" rows="4"></textarea>
-		</p>
-		<p class="control-duration">
-			<label>Duration</label><input maxlength=5 type="text" value="" placeholder="00:00">
-		</p>
-		<p class="control-notes">
-			<label>Video Notes</label><textarea cols="30" rows="4"></textarea>
-		</p>
-		<p class="control-check-lock">
-			<input type="checkbox">
-			<label>Locked for guests?</label>
-		</p>
-		<hr>
-		<div class="control-options">
-			<div><a href="#" class="remove">Remove</a> <a href="#" class="cancel">Cancel</a></div>
-			<div><a href="#" class="preview">Preview</a></div>
-		</div>
-	</div>
-</li>`;
+var template = '\n\
+<li class="item">\n\
+	<div class="links my-handle menu-item-handle">\n\
+		<a href="#" class="item-link"></a>\n\
+		<a href="#" class="item-arrow down"></a>\n\
+	</div>\n\
+	<div class="controls hide">\n\
+		<p class="control-name">\n\
+			<label>Item name</label><input type="text" value=""/>\n\
+		</p>\n\
+		<p class="control-check-header">\n\
+			<input type="checkbox"><label>Is header?</label>\n\
+		</p>\n\
+		<p class="control-code">\n\
+			<label>Video Embebed code</label><textarea cols="30" rows="4"></textarea>\n\
+		</p>\n\
+		<p class="control-duration">\n\
+			<label>Duration</label><input maxlength=5 type="text" value="" placeholder="00:00">\n\
+		</p>\n\
+		<p class="control-notes">\n\
+			<label>Video Notes</label><textarea cols="30" rows="4"></textarea>\n\
+		</p>\n\
+		<p class="control-check-lock">\n\
+			<input type="checkbox">\n\
+			<label>Locked for guests?</label>\n\
+		</p>\n\
+		<hr>\n\
+		<div class="control-options">\n\
+			<div><a href="#" class="remove">Remove</a> <a href="#" class="cancel">Cancel</a></div>\n\
+			<div><a href="#" class="preview">Preview</a></div>\n\
+		</div>\n\
+	</div>\n\
+</li>';
 
 
+
+
+(function( $ ) {
+	'use strict';
+
+	var obj_items = null; // existent objects
+	var items = null; // dynamic items objects
+
+	$( document ).ready(function() {
+
+		if ( $('#content').length > 0 && $('#content').text() ){
+			obj_items = JSON.parse($('#content').text());
+		}
+
+
+
+		items = $('#list-items');
+
+		if ( items.length > 0 ){
+
+			items.click( function(e){
+
+				e.preventDefault();
+ 
+			});
+
+		}
+
+
+
+		if ( obj_items ){
+			obj_items.forEach( function( obj_item, index) {
+				add_course( obj_item );
+			});
+		}
+		else{
+			add_empty_class();
+		}
+
+
+
+
+	}); //ready
+
+
+
+
+	// function for adding new course
+	function add_course( obj_item = null ){
+
+		items.append(template);
+		
+		var item = items.last();
+		var count = items.children().length;
+		console.log(item, count);
+		// update_ids(item, count - 1);
+
+		// // update values
+		// if ( obj_item ){
+		// 	update_values( item, obj_item );
+		// }
+		// else{
+		// 	item.querySelector('.links .item-link').innerText = `Untitled-${count}`;
+		// 	item.querySelector('.control-name input').value = `Untitled-${count}`;
+		// 	item.querySelector('.control-options .cancel').style.display = 'none';
+		// }
+	}
+
+	// add empty square
+	function add_empty_class( empty = true){
+		if ( empty )
+			$('#list-items').addClass('empty');
+		else
+			$('#list-items').removeClass('empty');
+	}
+
+
+	// function to udpate ids attributes for the HTML item
+	function update_ids( item, id){
+		console.log(item);
+		item.setAttribute('data-id', id);
+		item.querySelector('.control-name label').setAttribute('for', `name-${id}`);
+		item.querySelector('.control-name input').setAttribute('id', `name-${id}`);
+		item.querySelector('.control-check-header label').setAttribute('for', `isheader-${id}`);
+		item.querySelector('.control-check-header input').setAttribute('id', `isheader-${id}`);
+		item.querySelector('.control-code label').setAttribute('for', `code-${id}`);
+		item.querySelector('.control-code textarea').setAttribute('id', `code-${id}`);
+		item.querySelector('.control-duration label').setAttribute('for', `duration-${id}`);
+		item.querySelector('.control-duration input').setAttribute('id', `duration-${id}`);
+		item.querySelector('.control-notes label').setAttribute('for', `notes-${id}`);
+		item.querySelector('.control-notes textarea').setAttribute('id', `notes-${id}`);
+		item.querySelector('.control-check-lock label').setAttribute('for', `islock-${id}`);
+		item.querySelector('.control-check-lock input').setAttribute('id', `islock-${id}`);
+	}
+
+
+
+})( jQuery );
+
+
+
+
+
+/*
 let obj_items = null; // existent objects
 let items = null; // dynamic items objects
 
@@ -51,12 +155,16 @@ window.addEventListener('load', function(){
 
 		// Click Events for the link show/hide controls
 		items.addEventListener('click', function(e) {
-			
+		
+			// var scrollx = document.documentElement.scrollTop;
+
 			if ( ! e.target ) return;
+
+			//e.target.preventDefault();
 
 			// show/hide controls link
 			if ( e.target.matches('.item .item-arrow') ){
-				
+
 				let item = e.target.parentNode.parentNode;
 
 				const link = item.querySelector('.links .item-arrow');
@@ -68,6 +176,8 @@ window.addEventListener('load', function(){
 				if ( ! link.classList.contains('down') ) {
 					close_opens(item.dataset.id);
 				}
+
+				e.target.preventDefault();
 
 			}
 
@@ -147,6 +257,8 @@ window.addEventListener('load', function(){
 
 			}// preview - link
 
+			// window.scroll(100,scrollx);
+			// console.log(scrollx);
 
 		} ); // click event listener
 
@@ -357,6 +469,6 @@ function update_object(){
 
 
 
-
+*/
 
 
